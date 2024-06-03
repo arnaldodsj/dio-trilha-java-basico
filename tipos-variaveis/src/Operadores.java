0b16b3f8cecc4403543ebfc19055d8e114ad5a3d
Arnaldo dutra <arnaldodsj@gmail.com>
2024-06-03 10:45:24 -0300

    Operadorer Ternário1

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,23 @@
 public class Operadores {
     public static void main(String[] args) {
+        // int a, b;
+        // a = 6;
+        // b = 6;
+
+        // String resultado = a == b ? "verdadeiro" : "falso";
+
+        // System.out.println(resultado);
+
+
         int a, b;
-        a = 6;
+        a = 5;
         b = 6;
 
-        String resultado = a == b ? "verdadeiro" : "falso";
+        int resultado = a == b ? 1 : 0;
 
         System.out.println(resultado);
+
+
 
     }
 
